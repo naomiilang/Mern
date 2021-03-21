7a88354aacaa62e3c8eda13487ade45519869664
Naomi <naomilang1212@gmail.com>
2021-03-20 21:59:38 -0700

    basic typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book { 
+        bookId: 
+        authors:
+        description:
+        title:
+        image:
+        link:
+    }
+    type Auth {
+        token:
+        user: {User}
+    }
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+      }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
+    }
+`
+
+module.exports = { typeDefs };
