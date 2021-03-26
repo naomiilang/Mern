398fcd3119b2c778394bc48f42d203a81a1dc6e2
Naomi <naomilang1212@gmail.com>
2021-03-25 18:47:28 -0700

    trying to fix weird token error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,36 +2,24 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type User {
-        _id: ID
-        username: String
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
+        _id: ID!
+        username: String!
+        email: String!
     }
-    type Book { 
-        bookId: 
-        authors:
-        description:
-        title:
-        image:
-        link:
-    }
+
     type Auth {
         token: ID!
         user: User
     }
+
     type Query {
         me: User
-        users: [User]
-        user(username: String!): User
-      }
+    }
+
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(thoughtText: String!): Thought
-        addReaction(thoughtId: ID!, reactionBody: String!): Thought
-        addFriend(friendId: ID!): User
     }
-`
+`;
 
-module.exports = { typeDefs };
+module.exports = typeDefs;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = 'hiworld';
+const expiration = '10h';
 
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function ({req}) {
+    console.log(req.body)
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,14 +17,13 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
-
     // verify token and get user data out of it
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(token, 'hiworld', { maxAge: expiration });
       req.user = data;
-    } catch {
+    } catch(err) {
       console.log('Invalid token');
       return res.status(400).json({ message: 'invalid token!' });
     }
